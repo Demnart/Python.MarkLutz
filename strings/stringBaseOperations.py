4ac7b3edde6295d248974526df9f2d6e9b18619a
Артем <insidia93@gmail.com>
2017-12-13 21:55:07 +0200

    Strings BaseTypes

---
+++ strings/stringBaseOperations.py
@@ -0,0 +1,27 @@
+a = "hello"
+print(len(a))
+print(a + ", world!!!")
+print(a * 4)
+
+print("#"*20)
+
+myjob = "hacker"
+
+for i in myjob:
+    print(i,end="")
+print()
+print("k" in myjob)
+print("z" in myjob)
+print("spam" in "abcdspamefg")
+
+print("#"*20)
+
+s = "spam"
+
+print(s[0])
+print(s[:])
+print(s[0: -1])
+print(s[: -1])
+print(s[0:2],s[2:3],s[1:3])
+
+
