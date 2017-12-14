0e326a6e90052064bd27ebab1547fb23cf8c9aa9
genjo <insidia93@gmail.com>
2017-12-14 07:26:18 +0200

    numbers

---
+++ Numbers/Random.py
@@ -0,0 +1,7 @@
+import random
+
+print(random.random())
+
+print(random.randint(1,10))
+
+print(random.choice(["hello,world","It's my life","Hero"]))
---
+++ Numbers/firstExample.py
@@ -0,0 +1,19 @@
+import  math
+
+#Create two variable
+a = 3
+b = 4
+#Константы
+print(math.pi,math.e)
+#Син, кос, катангенс
+print(math.sin(2*math.pi/180))
+#Квадратный корень
+print(math.sqrt(28))
+#Возведение в степень
+print(pow(4,4),4**4)
+#Абсолютное значение, сумма
+print(abs(-41))
+print(sum((1,2,3,4)))
+#Максимум, минимум
+print(min(3,2,5,1))
+print(max(1,5,7,3))
