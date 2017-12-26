401f4df53753c788bbafe704470a56d6d2798c89
genjo <insidia93@gmail.com>
2017-12-26 12:41:25 +0200

    Работа с практическим примером.

---
+++ ClassRealExample/person.py
@@ -0,0 +1,22 @@
+"""Создаем наш класс Person для учебных целей и для получения практики раб
+оты с классами и ООП вцелом"""
+
+class Person:
+    """ """
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+
+    def percent(self,percent):
+        self.pay = int(self.pay * (1 + percent))
+if __name__ == "__main__" :
+
+    bob = Person("Bob Smith")
+    sue = Person("Sue","Manager",1500)
+    elise = Person("Elise","Developer")
+    print(bob.name,bob.job,bob.pay)
+    print(sue.name,sue.job,sue.pay)
+    print(sue.lastName(),bob.lastName())
